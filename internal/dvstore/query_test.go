4285545508329bea34f861cd303b62747cc86b64
Manfred Touron <m@42.am>
2019-12-03 22:54:06 +0100

    chore: unit test store

---
+++ internal/dvstore/query_test.go
@@ -0,0 +1,8 @@
+package dvstore
+
+import "testing"
+
+func TestLoadTasks(t *testing.T) {
+	// tasks, err := LoadTasks(h *cayley.Handle, schema *schema.Config, filters LoadTasksFilters, logger *zap.Logger)
+
+}
