fae2ce845b3c1757084241735cbf7bda80ccc02c
Lucas Germano <lucasgdev@gmail.com>
2020-03-02 13:27:28 -0300

    Add context as hook, add searchParams variable

--- web/src/components/menu/Menu.js
+++ web/src/components/menu/Menu.js
@@ -1,16 +1,22 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-useless-catch */
 
-import React from "react";
+import React, { useEffect, useContext } from "react";
 import { useForm } from "react-hook-form";
 import StoreContext from "../../store";
 import { fetchDepviz } from "../../api/depviz";
 import "./menu.scss";
 
 const Menu = () => {
-  const { register, getValues, handleSubmit } = useForm();
+  let AppStoreContext = useContext(StoreContext)
+  const { updateApiData, updateLayout } = AppStoreContext
+  const { register, getValues, setValue, handleSubmit } = useForm();
+  let searchParams = new URLSearchParams(window.location.search)
 
-  const onSubmit = async (data, updateApiData) => {
+  useEffect(() => {
+  }, [])
+
+  const onSubmit = async (data) => {
       const {
         targets,
         withClosed,
@@ -31,52 +37,66 @@ const Menu = () => {
       }
   }
 
+  const makeAPICall = async () => {
+    const {
+      targets,
+      withClosed,
+      withIsolated,
+      withPrs,
+      withExternalDeps,
+      layout
+    } = data;
+
+    // construct url
+    let url = `/graph?${targets.split(",").map(target => `targets=${target.trim()}`).join("&")}&withClosed=${withClosed}&withIsolated=${withIsolated}&withPrs=${withPrs}&withoutExternal-deps=${withExternalDeps}&layout=${layout}`
+
+    try {
+      const response = await fetchDepviz(url)
+      updateApiData(response.data, layout)
+    } catch (e) {
+      throw e;
+    }
+  }
+
   return (
-      <StoreContext.Consumer>
-        {({ updateApiData, updateLayout }) => {
-          return(
-            <form onSubmit={handleSubmit(data => onSubmit(data, updateApiData))}>
-              <div className="form-group">
-                <label htmlFor="targets">Repository:</label>
-                <input ref={register} type="text" name="targets" id="targets" />
-              </div>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div className="form-group">
+        <label htmlFor="targets">Repository:</label>
+        <input ref={register} type="text" name="targets" id="targets" />
+      </div>
 
-              <div className="form-group">
-                <input ref={register} type="checkbox" name="withClosed" id="withClosed" onChange={() => onSubmit(getValues(), updateApiData)} />
-                <label htmlFor="withClosed">Closed</label>
+      <div className="form-group">
+        <input ref={register} type="checkbox" name="withClosed" id="withClosed" onChange={() => onSubmit(getValues(), updateApiData)} />
+        <label htmlFor="withClosed">Closed</label>
 
-                <input ref={register} defaultChecked type="checkbox" name="withIsolated" id="withIsolated" onChange={() => onSubmit(getValues(), updateApiData)} />
-                <label htmlFor="withIsolated">Isolated</label>
+        <input ref={register} defaultChecked type="checkbox" name="withIsolated" id="withIsolated" onChange={() => onSubmit(getValues(), updateApiData)} />
+        <label htmlFor="withIsolated">Isolated</label>
 
-                <input ref={register} defaultChecked type="checkbox" name="withPrs" id="withPrs" onChange={() => onSubmit(getValues(), updateApiData)} />
-                <label htmlFor="withPrs">PRs</label>
+        <input ref={register} defaultChecked type="checkbox" name="withPrs" id="withPrs" onChange={() => onSubmit(getValues(), updateApiData)} />
+        <label htmlFor="withPrs">PRs</label>
 
-                <input ref={register} defaultChecked type="checkbox" name="withExternalDeps" id="withExternalDeps" onChange={() => onSubmit(getValues(), updateApiData)} />
-                <label htmlFor="withExternalDeps">Ext. Deps</label>
-              </div>
+        <input ref={register} defaultChecked type="checkbox" name="withExternalDeps" id="withExternalDeps" onChange={() => onSubmit(getValues(), updateApiData)} />
+        <label htmlFor="withExternalDeps">Ext. Deps</label>
+      </div>
 
-              <div className="form-group">
-                <label htmlFor="layout">Layout:</label>
-                <select ref={register} name="layout" id="layout" onChange={e => updateLayout(e.target.value)}>
-                  <option value="circle">circle</option>
-                  <option value="cose">cose</option>
-                  <option value="breadthfirst">breadthfirst</option>
-                  <option value="concentric">concentric</option>
-                  <option value="grid">grid</option>
-                  <option value="random">random</option>
-                  <option value="cola">cola</option>
-              {/*<option value="elk">elk</option>*/}
+      <div className="form-group">
+        <label htmlFor="layout">Layout:</label>
+        <select ref={register} name="layout" id="layout" onChange={e => updateLayout(e.target.value)}>
+          <option value="circle">circle</option>
+          <option value="cose">cose</option>
+          <option value="breadthfirst">breadthfirst</option>
+          <option value="concentric">concentric</option>
+          <option value="grid">grid</option>
+          <option value="random">random</option>
+          <option value="cola">cola</option>
+          {/*<option value="elk">elk</option>*/}
+        </select>
+      </div>
 
-                </select>
-              </div>
-
-              <div className="button-group">
-                <button type="submit">Generate</button>
-              </div>
-            </form>
-          )
-        }}
-      </StoreContext.Consumer>
+      <div className="button-group">
+        <button type="submit">Generate</button>
+      </div>
+    </form>
   )
 }
 
