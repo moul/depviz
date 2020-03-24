b363bd2441e75d018251fc313f8c1d8b603de8e4
Eugene Sysmanov <appigram@gmail.com>
2020-03-24 18:55:41 +0300

    Fix #269

--- web/src/api/index.js
+++ web/src/api/index.js
@@ -1,6 +1,8 @@
 import axios from 'axios'
 import store from '../utils/store'
 
+let retryCounter = 0
+
 const baseApi = axios.create({
   baseURL: process.env.API_URL,
 })
@@ -9,8 +11,7 @@ const baseApi = axios.create({
 baseApi.interceptors.request.use(
   (config) => {
     const newConfig = config
-    const auth = store.getItem('auth_token')
-    newConfig.headers.Authorization = `Basic ${btoa(`depviz:${auth}`)}`
+    newConfig.headers.Authorization = `Basic ${btoa(`depviz:${store.getItem('auth_token')}`)}`
     return newConfig
   },
 )
@@ -22,11 +23,14 @@ baseApi.interceptors.response.use((response) => response,
     const status = newError.response ? newError.response.status : null
 
     if (status === 401) {
-      const auth = process.env.AUTH_TOKEN
-      store.setItem('auth_token', auth)
-      newError.config.headers.Authorization = `Basic ${btoa(`depviz:${auth}`)}`
-      return baseApi.request(newError.config)
-    // });
+      if (process.env.AUTH_TOKEN) {
+        store.setItem('auth_token', process.env.AUTH_TOKEN)
+        newError.config.headers.Authorization = `Basic ${btoa(`depviz:${process.env.AUTH_TOKEN}`)}`
+      }
+      retryCounter += 1
+      if (retryCounter < 4) { // Allow 3 attempts to request
+        return baseApi.request(newError.config)
+      }
     }
     console.error('failed', newError, status, newError)
     alert(`failed: ${newError}`)
