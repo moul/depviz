40222d507f810c73434df06bab2ecb4c4a93e33d
Manfred Touron <94029+moul@users.noreply.github.com>
2022-07-23 15:33:46 +0000

    chore: fix go lint
    Signed-off-by: Manfred Touron <94029+moul@users.noreply.github.com>

--- tool/docker-protoc/tools.go
+++ tool/docker-protoc/tools.go
@@ -5,4 +5,5 @@ import (
 	_ "github.com/grpc-ecosystem/grpc-gateway/protoc-gen-grpc-gateway"
 	_ "github.com/grpc-ecosystem/grpc-gateway/protoc-gen-swagger"
 	_ "github.com/simplealpine/json2yaml"
+	_ "golang.org/x/tools/cmd/goimports"
 )
