5ea653bcf363d0d1594a0f090dfb248aed833581
Manfred Touron <94029+moul@users.noreply.github.com>
2022-07-23 08:08:46 +0000

    chore: bump docker-protoc
    Signed-off-by: Manfred Touron <94029+moul@users.noreply.github.com>

---
+++ tool/docker-protoc/tools.go
@@ -0,0 +1,8 @@
+package tools
+
+import (
+	_ "github.com/gogo/protobuf/protoc-gen-gogofaster"
+	_ "github.com/grpc-ecosystem/grpc-gateway/protoc-gen-grpc-gateway"
+	_ "github.com/grpc-ecosystem/grpc-gateway/protoc-gen-swagger"
+	_ "github.com/simplealpine/json2yaml"
+)
